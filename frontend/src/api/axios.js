f30585d0a429bd85371b4eae1a6390410033dc08
affanansarii <affanansarii7860@gmail.com>
2025-08-14 02:48:57 +0530

    updated api endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://good-reads-clone-backend.vercel.app/api",
+    baseURL: "https://good-reads-clone-backend.vercel.app",
     withCredentials: true //
 });
 
